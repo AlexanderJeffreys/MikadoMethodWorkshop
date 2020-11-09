f9b24e89fc96b6273b3f34718cda3f0ab2e133fc
Alex Jeffreys <alex.jeffreys@red-gate.com>
2020-11-09 18:19:01 +0000

    Change planet size based on mass

--- Space/PhysicalObject.cs
+++ Space/PhysicalObject.cs
@@ -44,7 +44,9 @@ namespace Space
                 var diameter = Mass >= SpaceRunner.EarthWeight * 10000 ? 7 : 2;
                 var xtmp = (int) ((X - SpaceRunner.CenterX) / SpaceRunner.Scale + SpaceRunner.Canvas.Width / 2);
                 var ytmp = (int) ((Y - SpaceRunner.CenterY) / SpaceRunner.Scale + SpaceRunner.Canvas.Height / 2);
-                
+
+                _shape.Height = diameter;
+                _shape.Width = diameter;
                 Canvas.SetTop(_shape, xtmp - diameter/2);
                 Canvas.SetLeft(_shape, ytmp - diameter/2);
             }
